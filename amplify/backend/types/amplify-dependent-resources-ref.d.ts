2346fe2aa1087c5fb8649ee98714d2cca9aa4872
QuanWa89 <qw.drylab@gmail.com>
2022-01-04 20:43:18 -0500

    add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,23 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytest": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "containera787faf9": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "ApiName": "string",
+            "RootUrl": "string"
+        }
+    }
+}
